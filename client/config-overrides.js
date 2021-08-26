55fe7bd2156b0b7be034433e60f8e2abf46c7ada
Daniel Manser <daniel.manser@awv-informatik.ch>
2021-08-26 10:12:02 +0200

    Changed some deps to get webpack server back again

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,20 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const {
-  addWebpackAlias,
   removeModuleScopePlugin,
-  babelInclude,
   fixBabelImports,
   addLessLoader,
   override,
   disableEsLint,
   addWebpackModuleRule,
 } = require('customize-cra')
-const { addReactRefresh } = require('customize-cra-react-refresh')
 
 module.exports = (config, env) =>
   override(
     addWebpackModuleRule({ test: /\.(of1|stp)$/, use: 'arraybuffer-loader' }),
-    addReactRefresh({}),
     disableEsLint(),
     removeModuleScopePlugin(),
     fixBabelImports('import', { libraryName: 'antd', libraryDirectory: 'es', style: true }),
