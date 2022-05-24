b32a2293c40d6cdd06136f362ca2142cbce28578
Daniel Manser <daniel.manser@awv-informatik.ch>
2022-05-24 07:44:41 +0200

    Add an import example for solid api

---
+++ client/src/solid/models/import-step.ts
@@ -0,0 +1,16 @@
+import { ApiNoHistory } from '@buerli.io/headless'
+import * as THREE from 'three'
+import data from '../../shared/resources/ventil.stp'
+
+export const create = async (api: ApiNoHistory) => {
+  const importedIds = await api.import(data)
+  console.info(importedIds)
+  const geom = await api.createBufferGeometry(importedIds[0])
+  const mesh = new THREE.Mesh(
+    geom,
+    new THREE.MeshStandardMaterial({ transparent: true, opacity: 1, color: new THREE.Color('rgb(255, 120, 255)') }),
+  )
+  return [mesh]
+}
+
+export default create
