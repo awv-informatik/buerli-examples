d08d2c7f3db728e57a9f39bfa24b37e4f86e11e9
Roman Bruelisauer <roman.bruelisauer@awv-informatik.ch>
2022-07-29 09:43:53 +0200

    Fix gripper example

--- client/src/models/history/Gripper_Example.ts
+++ client/src/models/history/Gripper_Example.ts
@@ -1,8 +1,8 @@
-import { ApiHistory, solid } from '@buerli.io/headless'
+import { ApiHistory, history } from '@buerli.io/headless'
 import arraybuffer from '../../resources/gripperV2.of1'
 import { Create, ParamType } from '../../store'
 
-export const paramsMap: ParamType = {
+export const params: ParamType = {
   Width: 60,
   Height: 150,
   Distance: 20,
@@ -25,7 +25,7 @@ export const create: Create = async (apiType, params) => {
   return productId[0]
 }
 
-export const update = async (api: ApiHistory, productId: number, params: ParamType = paramsMap) => {
+export const update = async (api: ApiHistory, productId: number, params: ParamType) => {
   await api.setExpressions(
     productId,
     { name: 'W', value: params['Width'] },
@@ -35,6 +35,6 @@ export const update = async (api: ApiHistory, productId: number, params: ParamTy
   )
 }
 
-export const cad = new solid()
+export const cad = new history()
 
-export default { create, update, cad }
+export default { update, create, params, cad }
