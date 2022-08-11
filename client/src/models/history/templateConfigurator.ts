bc40f3b552581dd009a5df04ebab55f73a87bcf0
Roman Bruelisauer <roman.bruelisauer@awv-informatik.ch>
2022-08-11 07:35:22 +0200

    Update templates

--- client/src/models/history/templateConfigurator.ts
+++ client/src/models/history/templateConfigurator.ts
@@ -4,9 +4,11 @@ import { Param, Create, Update, storeApi } from '../../store'
 
 export const paramsMap: Param[] = [
   // number example
-  // { index: 0, name: 'Width', type: 'number', value: 60 },
+  // { index: 0, name: 'test', type: ParamType.Number, value: 60 },
   // string example
-  // { section, index: 6, name: 'test', type: 'enum', value: 't1', values: ['t2', 't3', 't4'] },
+  // { index: 1, name: 'test', type: ParamType.Enum, value: 't1', values: ['t2', 't3', 't4'] },
+  // slider example
+  // { index: 2, name: 'test', type: ParamType.Slider, value: 6, step: 1, values: [2, 12] },
 ].sort((a, b) => a.index - b.index)
 
 export const create: Create = async (apiType, params) => {
