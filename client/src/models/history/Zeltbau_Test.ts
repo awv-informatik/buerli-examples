cb97c6bde069967bf7e15d33477928d3c6f33974
Roman Bruelisauer <roman.bruelisauer@awv-informatik.ch>
2024-01-10 17:28:50 +0100

    Update zelt example

--- client/src/models/history/Zeltbau_Test.ts
+++ client/src/models/history/Zeltbau_Test.ts
@@ -9,6 +9,7 @@ const zTiefe = 1
 const zLaenge = 2
 const fLinks = 3
 const fRechts = 4
+const posTuereVorne = 5
 const originPt = { x: 0, y: 0, z: 0 }
 const xDir = { x: 1, y: 0, z: 0 }
 const yDir = { x: 0, y: 1, z: 0 }
@@ -18,16 +19,26 @@ const originTransform: Transform = [originPt, xDir, yDir]
 const fMiddle = 2670
 const fBreite = 1100
 const fHoehe = 700
+const fEinbauhoehe = 1900
 
-let zeltErkerPrt: number
-let zeltEinfachPrt: number
-let fensterPrt: number
+// Default Werte
+const defaultBreiteZelt = 4000
+const defaultLaengeZelt = 10000
+const defaultErkerBreite = 2670
+
 let dachPrt: number
 
 let zeltInstance: number
 let dachInstance: number
 let fensterLinksInstances: number[] = []
 let fensterRechtsInstances: number[] = []
+let tuereVorneInstance: number
+
+let sL_Part: number
+let sR_Part: number
+let vW_Part: number
+let fenster110x70_Part: number
+let tuere189x127_Part: number
 
 export const paramsMap: Param[] = [
   { index: zType, name: 'Type', type: ParamType.Enum, value: 0, values: [0, 1, 2, 3] },
@@ -35,6 +46,7 @@ export const paramsMap: Param[] = [
   { index: zLaenge, name: 'Zelt Länge', type: ParamType.Slider, value: 10000, step: 100, values: [3000, 10000] },
   { index: fLinks, name: 'Anzahl Fenster links', type: ParamType.Enum, value: 0, values: [0, 1, 2, 3] },
   { index: fRechts, name: 'Anzahl Fenster rechts', type: ParamType.Enum, value: 0, values: [0, 1, 2, 3] },
+  { index: posTuereVorne, name: 'Position Tuere', type: ParamType.Number, value: 4360 },
 ].sort((a, b) => a.index - b.index)
 
 export const create: Create = async (apiType, params) => {
@@ -49,6 +61,13 @@ export const create: Create = async (apiType, params) => {
   const rootAsm = root ? root[0] : null
 
   if (rootAsm) {
+
+    ;[sL_Part] = await api.getPartTemplate('SL_Part_Ident')
+    ;[sR_Part] = await api.getPartTemplate('SR_Part_Ident')
+    ;[vW_Part] = await api.getPartTemplate('VW_Part_Ident')
+    ;[fenster110x70_Part] = await api.getPartTemplate('Fenster110x70_Ident')
+    ;[tuere189x127_Part] = await api.getPartTemplate('Tuere189x128_Ident')
+
     // ;[zeltErkerPrt] = await api.getPartTemplate('Zelt_Erker')
     // ;[zeltEinfachPrt] = await api.getPartTemplate('Zelt_Einfach')
     // ;[dachPrt] = await api.getPartTemplate('Dach')
@@ -73,57 +92,16 @@ export const update: Update = async (apiType, productId, params) => {
 
   const check = (param: Param) => typeof updatedParamIndex === 'undefined' || param.index === updatedParamIndex
 
-  // Change zelt
-  if (check(paramsMap[zType])) {
-    if (zeltInstance) {
-      await api.removeInstances({ id: zeltInstance })
-    }
-    if (params.values[zType] === 0) {
-      ;[zeltInstance] = await api.addInstances({
-        productId: zeltEinfachPrt,
-        ownerId: productId,
-        transformation: originTransform,
-        name: 'Zelt_Einfach_Inst',
-      })
-    } else if (params.values[zType] === 1) {
-      ;[zeltInstance] = await api.addInstances({
-        productId: zeltErkerPrt,
-        ownerId: productId,
-        transformation: originTransform,
-        name: 'Zelt_Erker_Inst',
-      })
-    }
-    const [originWCS] = await api.getWorkGeometry(zeltInstance, CCClasses.CCWorkCSys, 'Origin_WCS')
-    await api.createFastenedOriginConstraint(
-      productId,
-      { matePath: [zeltInstance], wcsId: originWCS, flip: FlipType.FLIP_Z, reoriented: ReorientedType.REORIENTED_0 },
-      0,
-      0,
-      0,
-      'FOC',
-    )
-    await updateZeltPrt(params.values, api)
-    await updateFensterLinks(params.values, api, productId)
-    await updateFensterRechts(params.values, api, productId)
-  }
-
-  // Change Zelt part
-  if (check(paramsMap[zTiefe]) || check(paramsMap[zLaenge])) {
-    await updateZeltPrt(params.values, api)
-    await updateFensterLinks(params.values, api, productId)
-    await updateFensterRechts(params.values, api, productId)
-  }
-
-  // Fenster links
-  if (check(paramsMap[fLinks])) {
-    await updateZeltPrt(params.values, api)
+  if(check(paramsMap[fLinks])) {
     await updateFensterLinks(params.values, api, productId)
   }
 
-  // Fenster rechts
-  if (check(paramsMap[fRechts])) {
-    await updateZeltPrt(params.values, api)
+  if(check(paramsMap[fRechts])) {
     await updateFensterRechts(params.values, api, productId)
+  }
+
+  if(check(paramsMap[posTuereVorne])) {
+    await updateTuereVorne(params.values, api, productId)
   }
 
   return productId
@@ -158,92 +136,93 @@ const updateZeltPrt = async (values: any[], api: ApiHistory) => {
   )
 }
 
+const updateTuereVorne = async (values: any[], api: ApiHistory, productId: number) => {
+  if (tuereVorneInstance) {
+    await api.removeInstances({ id: tuereVorneInstance})
+  }
+  // Create cutouts for windows
+  await api.setExpressions({ partId: vW_Part, members: [
+    { name: 'TuerVW_Offset', value: values[posTuereVorne] },
+  ]})
+
+  ;[tuereVorneInstance] = await api.addInstances({
+    productId: tuere189x127_Part,
+    ownerId: productId,
+    transformation: [
+      { x: 4360, y: 0, z: 0},
+      { x: 1, y: 0, z: 0},
+      { x: 0, y: -1, z: 0},
+    ],
+    name: 'Tuere_Vorne_Instance'
+  })
+}
+
 const updateFensterLinks = async (values: any[], api: ApiHistory, productId: number) => {
+  // Anzahl Fenster und deren Offset berechnen
+  const anzFensterLinks = values[fLinks]
+  const yOffsetLinks = (defaultBreiteZelt - (anzFensterLinks * fBreite)) / (anzFensterLinks + 1)
+
   if (fensterLinksInstances.length > 0) {
     const toRemoveInstances = []
     fensterLinksInstances.map(id => toRemoveInstances.push({ id }))
     await api.removeInstances(...toRemoveInstances)
   }
 
-  // Anzahl Fenster und deren Offset berechnen
-  const zeltTiefe = values[zTiefe]
-  const anzFenster = values[fLinks]
-  const yOffset = (zeltTiefe - (anzFenster * fBreite)) / (anzFenster + 1)
+  // Create cutouts for windows
+  await api.setExpressions({ partId: sL_Part, members: [
+    { name: 'SL_0_yOffset', value: anzFensterLinks > 0 ? yOffsetLinks : defaultBreiteZelt + 5000 },
+    { name: 'SL_1_yOffset', value: anzFensterLinks > 1 ? (2 * yOffsetLinks) + fBreite : defaultBreiteZelt + 5000 },
+    { name: 'SL_2_yOffset', value: anzFensterLinks > 2 ? (3 * yOffsetLinks) + 2 * fBreite : defaultBreiteZelt + 5000 },
+  ]})
 
   // Create instances and at them at once
   const windowInstancesToAdd = []
   for (let index = 0; index < values[fLinks]; index++) {
     windowInstancesToAdd.push({
-      productId: fensterPrt,
+      productId: fenster110x70_Part,
       ownerId: productId,
       transformation: [
-        { x: 20, y: values[zTiefe] - (((index + 1) * yOffset) + index * fBreite), z: 1800 - fHoehe },
+        { x: 20, y: values[zTiefe] - (((index + 1) * yOffsetLinks) + index * fBreite), z: fEinbauhoehe - fHoehe },
         { x: 0, y: 0, z: 1 },
         { x: 0, y: 1, z: 0 }],
       name: 'Fenster_' + index + '_Links',
     })
   }
   fensterLinksInstances = await api.addInstances(...windowInstancesToAdd)
-  
-  // Connect the instances to the zelt with constraints
-  // for (let index = 0; index < windowLeftInstances.length; index++) {
-  //   const windowInst = windowLeftInstances[index]
-  //   const [fensterWCS] = await api.getWorkGeometry(windowInst, CCClasses.CCWorkCSys, 'WorkCSys')
-  //   const [seiteLinksWCS] = await api.getWorkGeometry(zeltInstance, CCClasses.CCWorkCSys, 'SL_' + index + '_WCS')
-  //   await api.createFastenedConstraint(
-  //     productId,
-  //     { matePath: [zeltInstance], wcsId: seiteLinksWCS, flip: FlipType.FLIP_Z, reoriented: ReorientedType.REORIENTED_0 },
-  //     { matePath: [windowInst], wcsId: fensterWCS, flip: FlipType.FLIP_Z, reoriented: ReorientedType.REORIENTED_0 },
-  //     0,
-  //     0,
-  //     0,
-  //     'FC_Fenster_' + index + '_Links',
-  //   )
-  // }
 }
 
 const updateFensterRechts = async (values: any[], api: ApiHistory, productId: number) => {
+  // Anzahl Fenster und deren Offset berechnen
+  const anzFensterRechts = values[fRechts]
+  const yOffsetRechts = (defaultBreiteZelt - (anzFensterRechts * fBreite)) / (anzFensterRechts + 1)
+
   if (fensterRechtsInstances.length > 0) {
     const toRemoveInstances = []
     fensterRechtsInstances.map(id => toRemoveInstances.push({ id }))
     await api.removeInstances(...toRemoveInstances)
   }
 
-  // Anzahl Fenster und deren Offset berechnen
-  const zeltTiefe = values[zTiefe]
-  const anzFenster = values[fRechts]
-  const yOffset = (zeltTiefe - (anzFenster * fBreite)) / (anzFenster + 1)
+  // Create cutouts for windows
+  await api.setExpressions({ partId: sR_Part, members: [
+    { name: 'SR_0_yOffset', value: anzFensterRechts > 0 ? yOffsetRechts : defaultBreiteZelt + 5000 },
+    { name: 'SR_1_yOffset', value: anzFensterRechts > 1 ? (2 * yOffsetRechts) + fBreite : defaultBreiteZelt + 5000 },
+    { name: 'SR_2_yOffset', value: anzFensterRechts > 2 ? (3 * yOffsetRechts) + 2 * fBreite : defaultBreiteZelt + 5000 },
+  ]})
 
   // Create instances and at them at once
   const windowInstancesToAdd = []
   for (let index = 0; index < values[fRechts]; index++) {
     windowInstancesToAdd.push({
-      productId: fensterPrt,
+      productId: fenster110x70_Part,
       ownerId: productId,
       transformation: [
-        { x: values[zLaenge] - 20, y: ((index + 1) * yOffset) + index * fBreite, z: 1800 - fHoehe },
+        { x: values[zLaenge] - 20, y: ((index + 1) * yOffsetRechts) + index * fBreite, z: fEinbauhoehe - fHoehe },
         { x: 0, y: 0, z: 1 },
         { x: 0, y: -1, z: 0 }],
       name: 'Fenster_' + index + '_Rechts',
     })
   }
   fensterRechtsInstances = await api.addInstances(...windowInstancesToAdd)
-  
-  // Connect the instances to the zelt with constraints
-  // for (let index = 0; index < windowLeftInstances.length; index++) {
-  //   const windowInst = windowLeftInstances[index]
-  //   const [fensterWCS] = await api.getWorkGeometry(windowInst, CCClasses.CCWorkCSys, 'WorkCSys')
-  //   const [seiteLinksWCS] = await api.getWorkGeometry(zeltInstance, CCClasses.CCWorkCSys, 'SL_' + index + '_WCS')
-  //   await api.createFastenedConstraint(
-  //     productId,
-  //     { matePath: [zeltInstance], wcsId: seiteLinksWCS, flip: FlipType.FLIP_Z, reoriented: ReorientedType.REORIENTED_0 },
-  //     { matePath: [windowInst], wcsId: fensterWCS, flip: FlipType.FLIP_Z, reoriented: ReorientedType.REORIENTED_0 },
-  //     0,
-  //     0,
-  //     0,
-  //     'FC_Fenster_' + index + '_Links',
-  //   )
-  // }
 }
 
 export const cad = new history()
--- client/src/models/history/Zeltbau_TestCreate.ts
+++ client/src/models/history/Zeltbau_TestCreate.ts
@@ -1,13 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ApiHistory, history, Transform } from '@buerli.io/headless'
 import { Param, Create, storeApi, Update } from '../../store'
-import aBTemplate from '../../resources/history/Zeltbau/ZeltTemplateCreation.ofb?buffer'
+import aBTemplate from '../../resources/history/Zeltbau/ZeltTemplateCreation_NewSketch.ofb?buffer'
+import fenster110x70AB from '../../resources/history/Zeltbau/Fenster110_70.ofb?buffer'
+import tuere189x128AB from '../../resources/history/Zeltbau/Tuere189_128.ofb?buffer'
 import { BooleanOperationType, CCClasses, ExtrusionType } from '@buerli.io/classcad'
 
 const originPt = { x: 0, y: 0, z: 0 }
 const xDir = { x: 1, y: 0, z: 0 }
 const yDir = { x: 0, y: 1, z: 0 }
 const originTransform: Transform = [originPt, xDir, yDir]
+const defaultBreiteZelt = 4000
+const defaultLaengeZelt = 10000
+const defaultErkerBreite = 2670
 
 export const paramsMap: Param[] = [].sort((a, b) => a.index - b.index)
 
@@ -22,6 +27,8 @@ export const create: Create = async (apiType, params) => {
   const rootAsm = await api.createRootAssembly('RootAsm', { ident: 'RootAsmIdent'})
 
   if (rootAsm) {
+    await api.loadProduct(fenster110x70AB, 'ofb', { ident: 'Fenster110x70_Ident'})
+    await api.loadProduct(tuere189x128AB, 'ofb', { ident: 'Tuere189x128_Ident'})
 
     const sL_Part = await createSL(api)
     await api.addInstances({ productId: sL_Part, ownerId: rootAsm, transformation: originTransform, name: 'SL_Instance', options: { ident: 'SL_Instance_Ident'}})
@@ -32,8 +39,17 @@ export const create: Create = async (apiType, params) => {
     const rW_Part = await createRW(api)
     await api.addInstances({ productId: rW_Part, ownerId: rootAsm, transformation: originTransform, name: 'RW_Instance', options: { ident: 'RW_Instance_Ident'}})
 
-    const vW_Part = await createVW_Einfach(api)
-    await api.addInstances({ productId: vW_Part, ownerId: rootAsm, transformation: originTransform, name: 'VW_Instance', options: { ident: 'VW_Instance_Ident'}})
+    const vW_Einfach_Part = await createVW_Einfach(api)
+    await api.addInstances({ productId: vW_Einfach_Part, ownerId: rootAsm, transformation: originTransform, name: 'VW_Instance', options: { ident: 'VW_Instance_Ident'}})
+
+    const vW_Winkelbau_Part = await createVW_Winkelbau(api)
+    //await api.addInstances({ productId: vW_Winkelbau_Part, ownerId: rootAsm, transformation: originTransform, name: 'VW_Winkelbau_Instance', options: { ident: 'VW_Winkelbau_Instance_Ident'}})
+
+    const vW_1Erker_Part = await createVW_1Erker(api)
+    //await api.addInstances({ productId: vW_1Erker_Part, ownerId: rootAsm, transformation: originTransform, name: 'vW_1Erker_Instance', options: { ident: 'vW_1Erker_Instance_Ident'}})
+    
+    const vW_2Erker_Part = await createVW_2Erker(api)
+    //await api.addInstances({ productId: vW_2Erker_Part, ownerId: rootAsm, transformation: originTransform, name: 'vW_2Erker_Instance', options: { ident: 'vW_2Erker_Instance_Ident'}})
 
     const dach_Part = await createDachGiebel(api)
     await api.addInstances({ productId: dach_Part, ownerId: rootAsm, transformation: originTransform, name: 'Dach_Instance', options: { ident: 'Dach_Instance_Ident'}})
@@ -118,6 +134,79 @@ const createVW_Einfach = async (api: ApiHistory) => {
   const fenster2 = await api.box(wand_Part, [wcs2], 'ExpressionSet.VWF0_B', 100, 'ExpressionSet.VWF0_H')
   const tuere = await api.box(wand_Part, [wcs], 100, 'ExpressionSet.VWT_B', 'ExpressionSet.VWT_H')
   await api.boolean(wand_Part, BooleanOperationType.SUBTRACTION, [wand_Slice, tuere, fenster0, fenster1, fenster2])
+  return wand_Part
+}
+
+const createVW_Winkelbau = async (api: ApiHistory) => {
+  const [wand_Part] = await api.loadProduct(aBTemplate, 'ofb', { ident: 'VW_Winkelbau_Part_Ident'})
+  await api.setExpressions({
+    partId: wand_Part, members: [
+      { name: 'FMLength', value: defaultErkerBreite },
+      { name: 'FRight', value: defaultLaengeZelt - defaultErkerBreite}
+    ]
+  })
+  const [wand_SketchRegion] = await api.getSketchRegion(wand_Part, 'VW_1Erker')
+  const wand_Extrusion = await api.extrusion(wand_Part, wand_SketchRegion, ExtrusionType.UP, 0, 4000, 0, [0,0,1], 1)
+  const [dach_SheetSR] = await api.getSketchRegion(wand_Part, 'DachSheet')
+  const dach_Sheet = await api.extrusion(wand_Part, dach_SheetSR, ExtrusionType.CUSTOM, -500, 6000, 0, [0,0,-1], 0)
+  const wand_Slice = await api.sliceBySheet(wand_Part, wand_Extrusion, dach_Sheet, true)
+  const [wcs] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'TuerVW')
+  const [wcs1] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'EL_1')
+  const tuere = await api.box(wand_Part, [wcs], 100, 'ExpressionSet.VWT_B', 'ExpressionSet.VWT_H')
+  const fenster1 = await api.box(wand_Part, [wcs1], 'ExpressionSet.EFF_B', 100, 'ExpressionSet.EFF_H')
+  await api.boolean(wand_Part, BooleanOperationType.SUBTRACTION, [wand_Slice, tuere, fenster1])
+  return wand_Part
+}
+
+const createVW_1Erker = async (api: ApiHistory) => {
+  const [wand_Part] = await api.loadProduct(aBTemplate, 'ofb', { ident: 'VW_1Erker_Part_Ident'})
+  await api.setExpressions({
+    partId: wand_Part, members: [
+      { name: 'FRight', value: defaultLaengeZelt - defaultErkerBreite}
+    ]
+  })
+  const [wand_SketchRegion] = await api.getSketchRegion(wand_Part, 'VW_1Erker')
+  const wand_Extrusion = await api.extrusion(wand_Part, wand_SketchRegion, ExtrusionType.UP, 0, 4000, 0, [0,0,1], 1)
+  const [dach_SheetSR] = await api.getSketchRegion(wand_Part, 'DachSheet')
+  const dach_Sheet = await api.extrusion(wand_Part, dach_SheetSR, ExtrusionType.CUSTOM, -500, 6000, 0, [0,0,-1], 0)
+  const wand_Slice = await api.sliceBySheet(wand_Part, wand_Extrusion, dach_Sheet, true)
+  const [wcs] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'TuerVW')
+  const [wcs0] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'EL_0')
+  const [wcs1] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'EL_1')
+  const [wcs2] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'EL_2')
+  const tuere = await api.box(wand_Part, [wcs], 100, 'ExpressionSet.VWT_B', 'ExpressionSet.VWT_H')
+  const fenster0 = await api.box(wand_Part, [wcs0], 'ExpressionSet.ELF_B', 100, 'ExpressionSet.ELF_H')
+  const fenster1 = await api.box(wand_Part, [wcs1], 'ExpressionSet.EFF_B', 100, 'ExpressionSet.EFF_H')
+  const fenster2 = await api.box(wand_Part, [wcs2], 'ExpressionSet.ERF_B', 100, 'ExpressionSet.ERF_H')
+  await api.boolean(wand_Part, BooleanOperationType.SUBTRACTION, [wand_Slice, tuere, fenster0, fenster1, fenster2])
+  return wand_Part
+}
+
+const createVW_2Erker = async (api: ApiHistory) => {
+  const [wand_Part] = await api.loadProduct(aBTemplate, 'ofb', { ident: 'VW_2Erker_Part_Ident'})
+  const [wand_SketchRegion] = await api.getSketchRegion(wand_Part, 'VW_1Erker')
+  const [wand2_SketchRegion] = await api.getSketchRegion(wand_Part, 'VW_2Erker')
+  const wand_Extrusion = await api.extrusion(wand_Part, wand_SketchRegion, ExtrusionType.UP, 0, 4000, 0, [0,0,1], 1)
+  const wand2_Extrusion = await api.extrusion(wand_Part, wand2_SketchRegion, ExtrusionType.UP, 0, 4000, 0, [0,0,1], 1)
+  const wand_Boolean = await api.boolean(wand_Part, BooleanOperationType.UNION, [wand_Extrusion, wand2_Extrusion])
+  const [dach_SheetSR] = await api.getSketchRegion(wand_Part, 'DachSheet')
+  const dach_Sheet = await api.extrusion(wand_Part, dach_SheetSR, ExtrusionType.CUSTOM, -500, 6000, 0, [0,0,-1], 0)
+  const wand_Slice = await api.sliceBySheet(wand_Part, wand_Boolean, dach_Sheet, true)
+  const [wcs] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'TuerVW')
+  const [wcsL0] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'EL_0')
+  const [wcsL1] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'EL_1')
+  const [wcsL2] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'EL_2')
+  const tuere = await api.box(wand_Part, [wcs], 100, 'ExpressionSet.VWT_B', 'ExpressionSet.VWT_H')
+  const fensterL0 = await api.box(wand_Part, [wcsL0], 'ExpressionSet.ELF_B', 100, 'ExpressionSet.ELF_H')
+  const fensterL1 = await api.box(wand_Part, [wcsL1], 'ExpressionSet.EFF_B', 100, 'ExpressionSet.EFF_H')
+  const fensterL2 = await api.box(wand_Part, [wcsL2], 'ExpressionSet.ERF_B', 100, 'ExpressionSet.ERF_H')
+  const [wcsR0] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'ER_0')
+  const [wcsR1] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'ER_1')
+  const [wcsR2] = await api.getWorkGeometry(wand_Part, CCClasses.CCWorkCSys, 'ER_2')
+  const fensterR0 = await api.box(wand_Part, [wcsR0], 'ExpressionSet.EL2F_B', 100, 'ExpressionSet.EL2F_H')
+  const fensterR1 = await api.box(wand_Part, [wcsR1], 'ExpressionSet.EF2F_B', 100, 'ExpressionSet.EF2F_H')
+  const fensterR2 = await api.box(wand_Part, [wcsR2], 'ExpressionSet.ER2F_B', 100, 'ExpressionSet.ER2F_H')
+  await api.boolean(wand_Part, BooleanOperationType.SUBTRACTION, [wand_Slice, tuere, fensterL0, fensterL1, fensterL2, fensterR0, fensterR1, fensterR2])
   return wand_Part
 }
 
