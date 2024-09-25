f77bd630554b14692f20e6f6e6ed972db35ca206
Daniel Manser <daniel.manser@awv-informatik.ch>
2024-09-25 15:35:24 +0200

    Use wasm build 20240925.1

--- client/src/initBuerli.ts
+++ client/src/initBuerli.ts
@@ -1,7 +1,7 @@
 import { init, WASMClient } from '@buerli.io/classcad'
 
 export const initBuerli = () => {
-  init(id => new WASMClient('https://awvstatic.com/classcad/dev/wasm/20240924.2', id), {
+  init(id => new WASMClient('https://awvstatic.com/classcad/dev/wasm/20240925.1', id), {
     config: { geometry: { points: { hidden: true }, edges: { color: 'black' } } },
   })
 }
