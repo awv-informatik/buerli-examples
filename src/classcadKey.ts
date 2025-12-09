ccb10e76e28435d6949076950a43f4ed67b26c5e
Daniel Manser <daniel.manser@awv-informatik.ch>
2025-12-09 12:08:32 +0100

    Update initBuerli

---
+++ src/classcadKey.ts
@@ -0,0 +1,6 @@
+// TODO: Insert your ClassCAD WASM key here to use ClassCAD WASM
+
+/**
+ * The ClassCAD key used to initialize the WASM build of ClassCAD.
+ */
+export const classcadKey = ''
--- src/initBuerli.ts
+++ src/initBuerli.ts
@@ -1,8 +1,14 @@
-import { init, SocketIOClient } from '@buerli.io/classcad'
+import { init, SocketIOClient, WASMClient } from '@buerli.io/classcad'
 import { CCSERVERURL } from './config'
 
-export const initBuerli = () => {
-  init(id => new SocketIOClient(CCSERVERURL, id), {
-    config: { geometry: { points: { hidden: true }, edges: { color: 'black' } } },
+import { classcadKey } from './classcadKey'
+
+export const initBuerli = async () => {
+  init(id => {
+    if (classcadKey) {
+      return new WASMClient(id, { appKey: classcadKey })
+    } else {
+      return new SocketIOClient(CCSERVERURL, id)
+    }
   })
 }
