3321ceedb39efeaffe1c2ec23aa317ebfba66ab6
Daniel Manser <daniel.manser@awv-informatik.ch>
2025-12-19 10:37:38 +0100

    Disable logToConsole

--- src/initBuerli.ts
+++ src/initBuerli.ts
@@ -9,7 +9,7 @@ const socketIoUrl = SOCKETIO_URL
 export const initBuerli = async () => {
   init(id => {
     if (classcadWasmKey) {
-      return new WASMClient(id, { classcadKey: classcadWasmKey, logToConsole: true })
+      return new WASMClient(id, { classcadKey: classcadWasmKey })
     } else {
       return new SocketIOClient(socketIoUrl, id)
     }
